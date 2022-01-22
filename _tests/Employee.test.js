b67f4312309bc69a8150a5d5a8b502d516d12b30
Dan Beck <danjacobbeck@gmail.com>
2022-01-21 16:40:32 -0800

    employee test

--- _tests/Employee.test.js
+++ _tests/Employee.test.js
@@ -1,1 +1,18 @@
 const Employee = require("../lib/Employee")
+
+test("positive employee instance", () => {
+    const e = new Employee();
+    expect(typeof(e)).toBe("object");
+});
+
+test("Can set name", () => {
+    const name = "Sophie";
+    const e = new Employee(name);
+    expect(e.name).toBe(name);
+});
+
+test("Can set ID", () => {
+    const testValue = 100;
+    const e = new Employee("foo", 1, testValue);
+    expect(e.email).toBe(testValue);
+});
