6c3ccad61edf59f956e2429cdacc5914415ddc6c
Dan Beck <danjacobbeck@gmail.com>
2022-01-21 16:52:46 -0800

    employee.test edits

--- _tests/Employee.test.js
+++ _tests/Employee.test.js
@@ -16,3 +16,33 @@ test("Can set ID", () => {
     const e = new Employee("foo", 1, testValue);
     expect(e.email).toBe(testValue);
 });
+
+test("Can set email", () => {
+    const testValue = "test@test.com";
+    const e = new Employee("foo", 1, testValue);
+    expect(e.email).toBe(testValue);
+});
+
+test("Can get name with getName()", () => {
+    const testValue = "Sophie";
+    const e = new Employee(testValue)
+    expect(e.getName()).toBe(testValue);
+});
+
+test("Can get ID with getId()", () => {
+    const testValue = 100;
+    const e = new Employee("foo", testValue);
+    expect(e.getId()).toBe(testValue);
+});
+
+test("Can get email with getEmail()", () => {
+    const testValue = "test@test.com";
+    const e = new Employee("foo", testValue);
+    expect(e.getEmail()).toBe(testValue);
+});
+
+test("getRole() => \"Employee\"", () => {
+    const testValue = "Employee";
+    const e = new Employee("Sophie", 1, "test@test.com");
+    expect(e.getRole()).toBe(testValue);
+});
